c40f1c62ddb17b23bf3ac595b48a7c2bb975e785
Danny ly <dannyly199@gmail.com>
2017-03-14 22:53:11 -0700

    comments finished for customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -8,19 +8,17 @@
 
 #include "customer.h"
 /*
- +==============================================================================-
+ +====================	<DISPLAY HISTORY >	====================================-
  ||
  ||   Function_Description:
- ||       -
+ ||       -This function  displays the history for this speicifc customer
  ||   Preconditions:
- ||       -
- ||       -
+ ||       -None
  ||   Postconditions:
- ||      -
- ||       -
- ||
+ ||      -Will output from the most recent borrow or return transaction to the 
+ ||			Console.
  ||   Assumptions:
- ||       -
+ ||       - The history vector has been intastalized.
  +==============================================================================-
  */
 void Customer::displayHistory() const {
@@ -38,60 +36,49 @@ void Customer::displayHistory() const {
         }else if(history[i]->getTransactionType() == 'R'){
             std::cout << " Returned " << history[i]->getMovieTitle() << std::endl;
         }
-        //history[i]->print();
-        //std::cout << history[i]->print() << std::endl;
     }
 };
 /*
- +==============================================================================-
- ||
+ +========================<ADD TRANSACTION ======================================-
  ||   Function_Description:
- ||       -
+ ||       - Helper function that adds a transcation to the customers transaction
+ ||			vector
  ||   Preconditions:
- ||       -
- ||       -
+ ||       - None
  ||   Postconditions:
- ||      -
- ||       -
- ||
+ ||      -Transaction pointer will be inserted into the history vector
  ||   Assumptions:
- ||       -
+ ||       - None
  +==============================================================================-
  */
 void Customer::addTransaction(Transaction *transaction){
     history.push_back(transaction);
 };
 /*
- +==============================================================================-
+ +======================	DEFAULT CONSTRUCTOR ===============================-
  ||
  ||   Function_Description:
- ||       -
+ ||       - default constructor used to initalize a defaulting customer object
  ||   Preconditions:
- ||       -
- ||       -
+ ||       - None
  ||   Postconditions:
- ||      -
- ||       -
- ||
+ ||      - Customer object will be initalitated.
  ||   Assumptions:
- ||       -
+ ||       - None
  +==============================================================================-
  */
 Customer::Customer(){};
 /*
- +==============================================================================-
+ +======================== PARAMETRIC DEFAULT CUSTRUCTOR =================-
  ||
  ||   Function_Description:
- ||       -
+ ||       - constructor used to make object given ID, firstname, and lastname
  ||   Preconditions:
- ||       -
- ||       -
+ ||       - all parameters must be of type string
  ||   Postconditions:
- ||      -
- ||       -
- ||
+ ||      - Customer object has been initalized.
  ||   Assumptions:
- ||       -
+ ||       - None
  +==============================================================================-
  */
 
@@ -101,135 +88,100 @@ Customer::Customer(std::string id, std::string firstName, std::string lastName){
     setCustomerLastName(lastName);
 };
 /*
- +==============================================================================-
+ +======================set CustomerID=========================================-
  ||
  ||   Function_Description:
- ||       -
+ ||       - helper function to set customer ID
  ||   Preconditions:
- ||       -
- ||       -
+ ||       - parameter must be valid string repersentation of customer ID
  ||   Postconditions:
- ||      -
- ||       -
+ ||      - Customer id will be set to the parameter
  ||
  ||   Assumptions:
- ||       -
+ ||       - Customer ID is a string type
  +==============================================================================-
  */
 void Customer::setCustomerID(std::string customerID){
     this->customerID = customerID;
 };
 /*
- +==============================================================================-
+ +====================set Customer FirstName===================================-
  ||
  ||   Function_Description:
- ||       -
+ ||       - sets the customers first name
  ||   Preconditions:
- ||       -
- ||       -
+ ||       -None
  ||   Postconditions:
- ||      -
- ||       -
- ||
+ ||      - the customers first name will be set to the parameter given
  ||   Assumptions:
- ||       -
+ ||       - None
  +==============================================================================-
  */
 void Customer::setCustomerFirstName(std::string firstName){
     this->firstName = firstName;
 };
 /*
- +==============================================================================-
+ +===========================SET CUSTOMER LAST NAME==================-
  ||
  ||   Function_Description:
- ||       -
+ ||       - sets the customers last name of the current object
  ||   Preconditions:
- ||       -
- ||       -
+ ||       -None
  ||   Postconditions:
- ||      -
- ||       -
- ||
+ ||      - lastname is set to the given parameter
  ||   Assumptions:
- ||       -
+ ||       - None
  +==============================================================================-
  */
 void Customer::setCustomerLastName(std::string lastName){
     this->lastName = lastName;
 };
 /*
- +==============================================================================-
+ +========================GET CUSTOMER ID ==================================-
  ||
  ||   Function_Description:
- ||       -
+ ||       -gets the current customer ID, as a string
  ||   Preconditions:
- ||       -
- ||       -
+ ||       - None
  ||   Postconditions:
- ||      -
- ||       -
+ ||      - Returns a string repersentation of the customers ID
  ||
  ||   Assumptions:
- ||       -
+ ||       - the calling function expects a string as return type 
  +==============================================================================-
  */
 std::string Customer::getCustomerID() const{
     return this->customerID;
 };
 /*
- +==============================================================================-
+ +==============================GET customer First name ========================-
  ||
  ||   Function_Description:
- ||       -
+ ||       - returns the customers first name
  ||   Preconditions:
- ||       -
- ||       -
+ ||       - None
  ||   Postconditions:
- ||      -
- ||       -
- ||
+ ||      - returns the customers first name
  ||   Assumptions:
- ||       -
+ ||       - None
  +==============================================================================-
  */
 std::string Customer::getCustomerFirstName() const{
     return this->firstName;
 };
 /*
- +==============================================================================-
+ +=========================get customer lastname================================-
  ||
  ||   Function_Description:
- ||       -
+ ||       - gets the current customer objecst lastname
  ||   Preconditions:
- ||       -
- ||       -
+ ||       - None
  ||   Postconditions:
- ||      -
- ||       -
- ||
+ ||      - a string of the customers lastname is returned 
  ||   Assumptions:
- ||       -
+ ||       - None
  +==============================================================================-
  */
 std::string Customer::getCustomerLastName() const{
     return this->lastName;
 };
-/*
- +==============================================================================-
- ||
- ||   Function_Description:
- ||       -
- ||   Preconditions:
- ||       -
- ||       -
- ||   Postconditions:
- ||      -
- ||       -
- ||
- ||   Assumptions:
- ||       -
- +==============================================================================-
- */
-
-
-
