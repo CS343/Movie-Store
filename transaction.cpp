2b3d56434f9a39702f34cb01ca191d3d8cf5f2d5
bardiaborhani <bardia.borhani@gmail.com>
2017-03-15 22:40:02 -0700

    Comments Added

--- transaction.cpp
+++ transaction.cpp
@@ -9,39 +9,50 @@
 
 #include "transaction.h"
 
+
 Transaction::Transaction(){
 
 }
 
 
-//getters
+// returns the actor's full name
+// only used for classic movies
 std::string Transaction::getActorName() const{
     return this->actorName;
 }
+
+// returns the movie's title
 std::string Transaction::getMovieTitle() const{
     return this->movieTitle;
 }
+
+// returns the customer's ID
 std::string Transaction::getCustomerID() const{
     return this->customerID;
 }
 
+// returns the trasaction type
 char Transaction::getTransactionType() const{
     return this->transactionType;
 }
 
-
+// returns the media type
 char Transaction::getMediaType() const{
     return this->mediaType;
 }
 
+// returns the movie genre
+// All going to be "D" for DVDs for this project
 char Transaction::getMovieGenre() const{
     return this->movieGenre;
 }
 
+// returns movie year
 int Transaction::getMovieYear() const{
     return this->movieYear;
 }
 
+// returns the movie director
 std::string Transaction::getMovieDirector() const{
     return this->movieDirector;
 }
@@ -50,49 +61,81 @@ int Transaction::getMovieReleasedMonth() const{
     return this->movieReleasedMonth;
 }
 
+// returns the actors first name
+// only used for classic movies
 std::string Transaction::getFirstName() const{
     return this->firstName;
 }
+
+// reutrns the actor's last name
+// only used for classic movies
 std::string Transaction::getLastName() const{
     return this->lastName;
 }
 
+
+
 //setters
 
+// sets the actors full name
+// only used for classic movies
 void Transaction::setActorName(std::string firstName, std::string lastName ){
     this->actorName = firstName + " " + lastName;
 }
+
+// sets the actors first name
+// only used for classic movies
 void Transaction::setFirstName(std::string first){
     this->firstName = first;
 }
+
+// sets the actors last name
+// only used for classic movies
 void Transaction::setLastName(std::string last){
     this->lastName = last;
 }
+
+// sets the release month of the movie
 void Transaction::setMovieReleasedMonth(std::string month){
     int value  = std::atoi(month.c_str());
     this->movieReleasedMonth = value;
 }
+
+// sets the name of the movie director
 void Transaction::setMovieDirector(std::string director){
     this->movieDirector = director;
 }
+
+// sets the media type
+// All going to be "D" for DVDs for this project
 void Transaction::setMediaType(char mediaType){
     this->mediaType = mediaType;
 }
 
-
+// sets the Transaction type
 void Transaction::setTransactionType(char type){
     this->transactionType = type;
 }
+
+// sets the movie title
 void Transaction::setMovieTitle(std::string movieTitle){
     this->movieTitle = movieTitle;
 }
+
+// sets the customer's ID number
 void Transaction::setCustomerID(std::string customerID){
     this->customerID = customerID;
 }
+
+// sets the movie genre
 void Transaction::setMovieGenre(char genre) {
     this->movieGenre = genre;
 }
 
+// sets the year the movie was released
+// converted from string to int. It was read as a string
+// when the ifstream variable was reading the lines in 
+// the movie file in Store's readMovies function
 void Transaction::setMovieYear(std::string year){
     
     int value = std::atoi(year.c_str());
@@ -100,17 +143,6 @@ void Transaction::setMovieYear(std::string year){
 }
 
 
-/*
-
-void Transaction::makeTransaction(std::string result, char transactionType){
-    //IM TRYING TO MAKE THIS METHOD A PURE VIRTUAL METHOD SUCH THAT
-    //THIS METHOS NEEDS TO BE DEFINED WITHIN THE
-    //SUBCLASSES
-     //NOT SURE WHY XCODE C++ DOES NOT ALLOW ME TO DO THIS
-    
-    std::cout << "stupid called" << std::endl;
-}
- */
 
 
  
