368f42b8af9a92e0e6ec0b10318a56a5b1ccf85e
bardiaborhani <bardia.borhani@gmail.com>
2017-03-15 21:12:18 -0700

    Comments Added

--- viewinventory.cpp
+++ viewinventory.cpp
@@ -2,8 +2,8 @@
 //  viewinventory.cpp
 //  movie Store
 //
-//  Created by Danny Ly on 3/9/17.
-//  Copyright © 2017 Danny Ly. All rights reserved.
+//  Created by Danny Ly and Bardia Borhani on 3/9/17.
+//  Copyright © 2017 Danny Ly and Bardia Borhani. All rights reserved.
 //
 
 
