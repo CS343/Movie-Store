f0f8441748a385b39facd0ec03fbbbcfa45f73da
Danny ly <dannyly199@gmail.com>
2017-03-15 20:37:53 -0700

    comments

--- store.cpp
+++ store.cpp
@@ -97,38 +97,51 @@ Store::Store(){
 
 bool Store::readMovies(ifstream& infile){
 /*
- This ReadMovies method works as follows , as you make a Movie parent class, given a command or action pass that to the Movie constructor
- which will return a movie type , now check what the movie type is and insert that movie into the corresponding binary tree storage
- -> inside the insert method of the binary tree there is a check that check wether a element has been found, if found, then increment
-    its quntitty, and check for success checker if not success means quntitt updateed and we delete the duplicate movie object
+ This ReadMovies method works as follows , as you make a Movie parent class, 
+ given a command or action pass that to the Movie constructor
+ which will return a movie type , now check what the movie type is and insert
+ that movie into the corresponding binary tree storage
+ -> inside the insert method of the binary tree there is a check that check 
+ wether a element has been found, if found, then increment
+    its quntitty, and check for success checker if not success means quntitt
+	updateed and we delete the duplicate movie object
  */
     string result;
     while(getline(infile,result)){
-        if(result.at(0) != 'C' && result.at(0) != 'D' && result.at(0) != 'F'){ //|| result.at(0) != 'F' || result.at(0) != 'D'){
-            //another approach is to get the first char, then pass the ret of the ifstream to a Movie object to populate itself
+        if(result.at(0) != 'C' && result.at(0) != 'D' && result.at(0) != 'F'){
+            //another approach is to get the first char, then pass the ret of
+			//the ifstream to a Movie object to populate itself
             cout << "ERROR: (Recieved an invalid command) " << result << endl;
             continue;
         }
         //used to split all items in the string by delimiter ',' coma
-        vector<string> split_movie_array = Helper_Functions::string_split(result, ',');
+        vector<string> split_movie_array = 
+			Helper_Functions::string_split(result, ',');
         
         Movie *moviePtr;
-                    //access the action which is the first in the array, accessing the zero element of this item gets us the char from the char array
+        //access the action which is the first in the array, accessing
+		//the zero element of this item gets us the char from the char array
         char action = split_movie_array[0].c_str()[0];
-        //c_str() makes strings into a char array(split), indexing the zero element give me the first split char
-        //call the movie factory to make a movie object either comedy, classic, or drama
+        //c_str() makes strings into a char array(split), indexing the zero 
+		//element give me the first split char
+        //call the movie factory to make a movie object either comedy, 
+		//classic, or drama
         //given an action and return it
         moviePtr = Movie_Factory::make_movie(action);
         //give the array with data to the respective object makeMovie method
         moviePtr->makeMovie(split_movie_array);
         
         //insert the movie object
-        //check for success if it was successful it was inserted if it was not
-        //it either was duplicate, so we need to increment the quntity and delete the made object
+        //check for success if it was successful it was inserted if it was
+		//not
+        //it either was duplicate, so we need to increment the quntity and 
+		//delete the made object
         
-        //a boolean that check weather an item was successfullly inserted into the
+        //a boolean that check weather an item was successfullly inserted 
+		//into the
         //respective databasess,
-        //if a duplicate was found or an error occurred then the new'd movie will be
+        //if a duplicate was found or an error occurred then the new'd
+		//movie will be
         //deleted to prevent memory leak
         bool success = false;
         switch (action) {
@@ -152,22 +165,21 @@ bool Store::readMovies(ifstream& infile){
 }
 
 
-/*
-+==============================================================================-
+
+/*==============================< FUNCTION NAME >==============================
+||	Function Desction:
+||		-
 ||
-||   Function_Description:
-||       -
-||   Preconditions:
-||       -
-||       -
-||   Postconditions:
-||      -
-||       -
+||	Precondition:
+||		-
+||		-
 ||
-||   Assumptions:
-||       -
-+==============================================================================-
-*/
+||	Precondition:
+||		-
+||		-
+||	Assumptions:
+||		-
+++===========================================================================*/
 
 bool Store::readCustomers(ifstream& infile){
     string id,firstName, lastName;
@@ -186,49 +198,25 @@ bool Store::readCustomers(ifstream& infile){
         	customerStorage.put(std::atoi(ptr->getCustomerID().c_str()),
 			ptr);
 		}
-		
-
-
-
-/*
-        //grab the customer data from file
-        infile >> id >> firstName >> lastName;
-        std::cout <<"DLETING: " << id << std::endl;
-		
-		//create the customer object given the data;
-        Customer *customerObj = new Customer(id, firstName, lastName);
-        
-        //insert the customer into the store object, and customer hashmap
-        
-        //customerHashTable.insert(customerObj->getCustomerID(), customerObj);
-        customerStorage.put(std::atoi(customerObj->getCustomerID().c_str()), customerObj);
-  */  
-/*
-        if (infile.eof()){
-			//delete customerObj;
-            break;    
-        }
-*/
 	}
     return true;
 }
 
-/*
- +==============================================================================-
- ||
- ||   Function_Description:
- ||       -
- ||   Preconditions:
- ||       -
- ||       -
- ||   Postconditions:
- ||      -
- ||       -
- ||
- ||   Assumptions:
- ||       -
- +==============================================================================-
- */
+/*==============================< FUNCTION NAME >==============================
+||	Function Desction:
+||		-
+||
+||	Precondition:
+||		-
+||		-
+||
+||	Precondition:
+||		-
+||		-
+||	Assumptions:
+||		-
+++===========================================================================*/
+
 bool Store::readTransactions(ifstream& infile){
     string result;
     
@@ -264,22 +252,20 @@ bool Store::readTransactions(ifstream& infile){
     return true;
 };
 
-/*
- +==============================================================================-
- ||
- ||   Function_Description:
- ||       -
- ||   Preconditions:
- ||       -
- ||       -
- ||   Postconditions:
- ||      -
- ||       -
- ||
- ||   Assumptions:
- ||       -
- +==============================================================================-
- */
+/*==============================< FUNCTION NAME >==============================
+||	Function Desction:
+||		-
+||
+||	Precondition:
+||		-
+||		-
+||
+||	Precondition:
+||		-
+||		-
+||	Assumptions:
+||		-
+++===========================================================================*/
 
 bool Store::doTransactions(){
     //HYE BARDIA
@@ -287,27 +273,12 @@ bool Store::doTransactions(){
     for(int trans = 0; trans < transactionStorage.size(); trans++){
         //get the current transaction given q FIFO requirment
         transPtr = this->transactionStorage[trans];
-        //called doTransaction from the current transaction Object(polymorphism)
-        transPtr->doTransaction(this->_classicStorage, this->_comedyStorage, this->_dramaStorage, this->customerStorage);
+        //called doTransaction from the current transaction Object(polymorphis)
+        transPtr->doTransaction(this->_classicStorage, this->_comedyStorage,
+			this->_dramaStorage, this->customerStorage);
     }
    
     return true;
     
 }
 
-/*
- +==============================================================================-
- ||
- ||   Function_Description:
- ||       -
- ||   Preconditions:
- ||       -
- ||       -
- ||   Postconditions:
- ||      -
- ||       -
- ||
- ||   Assumptions:
- ||       -
- +==============================================================================-
- */
