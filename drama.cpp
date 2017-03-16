4a0f6642ece561a1cbc580dd250dba029d75ea2c
Danny ly <dannyly199@gmail.com>
2017-03-16 01:34:39 -0700

    comments added

--- drama.cpp
+++ drama.cpp
@@ -8,7 +8,40 @@
 
 #include "drama.h"
 
+/*==========================< FUNCTION NAME >=================================
+ ||
+ ||   Function_Description:
+ ||      - Default constructor
+ ||
+ ||   Preconditions:
+ ||      - None
+ ||      -
+ ||   Postconditions:
+ ||      - Initalizes the object
+ ||      -
+ ||
+ ||   Assumptions:
+ ||      - None
+ ++===========================================================================*/
+
+
 Drama::Drama(){};
+/*==========================< FUNCTION NAME >=================================
+ ||
+ ||   Function_Description:
+ ||      - Operator < to tell if this drama is less than RHS, given the 
+ ||     director and title
+ ||
+ ||   Preconditions:
+ ||      - None
+ ||      -
+ ||   Postconditions:
+ ||      - Return true if this is less than RHS object
+ ||      -
+ ||
+ ||   Assumptions:
+ ||      - None
+ ++===========================================================================*/
 
 bool Drama::operator<(const Movie &rhs) const{
    // std::cout << "< called from Drama" << std::endl;
@@ -19,20 +52,24 @@ bool Drama::operator<(const Movie &rhs) const{
     }else{
         return getTitle() < rhs.getTitle();
     }
-    /*
-    if(this->getDirector() < rhs.getDirector()){
-        return true;
-    }else if(this->getDirector() == rhs.getDirector()){
-        if(this->getTitle() < rhs.getTitle()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    //return (this->getDirector() < rhs.getDirector()) && (this->getTitle() < rhs.getTitle());
-    return true;
-     */
+
 };
+/*==========================< FUNCTION NAME >=================================
+ ||
+ ||   Function_Description:
+ ||      - Operator > overload to see if this is greater than RHS, given
+ ||     the director and title
+ ||
+ ||   Preconditions:
+ ||      - None
+ ||      -
+ ||   Postconditions:
+ ||      - Returns true if this is greater than RHS
+ ||      -
+ ||
+ ||   Assumptions:
+ ||      - None
+ ++===========================================================================*/
 
 bool Drama::operator>(const Movie &rhs) const{
     // std::cout << "< called from Drama" << std::endl;
@@ -42,35 +79,70 @@ bool Drama::operator>(const Movie &rhs) const{
     }else{
         return getTitle() > rhs.getTitle();
     }
-    /*
-    if(this->getDirector() > rhs.getDirector()){
-        return true;
-    }else if(this->getDirector() == rhs.getDirector()){
-        if(this->getTitle() > rhs.getTitle()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    //return (this->getDirector() < rhs.getDirector()) && (this->getTitle() < rhs.getTitle());
-    return true;
-    
-    //return (this->getDirector() > rhs.getDirector()) && (this->getTitle() > rhs.getTitle());
-     */
+
 };
+/*==========================< FUNCTION NAME >=================================
+ ||
+ ||   Function_Description:
+ ||      - checks wether two drama objects are the same,
+ ||
+ ||   Preconditions:
+ ||      - None
+ ||      -
+ ||   Postconditions:
+ ||      - Returns true if the two ovjects are the same false otherwise
+ ||      -
+ ||
+ ||   Assumptions:
+ ||      - None
+ ++===========================================================================*/
 
 bool Drama::operator==(const Movie &rhs) const{
-    return (this->getDirector() == rhs.getDirector()) && (this->getTitle() == rhs.getTitle());
+    return (this->getDirector() == rhs.getDirector()) && (this->getTitle()
+                                                          == rhs.getTitle());
     /* && (this->getYear() == rhs.getYear()*/
 };
+/*==========================< FUNCTION NAME >=================================
+ ||
+ ||   Function_Description:
+ ||      - check wether two objects are not the same
+ ||
+ ||   Preconditions:
+ ||      - None
+ ||      -
+ ||   Postconditions:
+ ||      - If the objects are different returns true false other wise
+ ||      -
+ ||
+ ||   Assumptions:
+ ||      - None
+ ++===========================================================================*/
 
 bool Drama::operator!=(const Movie &rhs) const{
     return !(*this == rhs);
 };
 /*
- Given an vector/array of data, populate the current movie object with its respective elements
+ Given an vector/array of data, populate the current movie object with i
+ ts respective elements
  
  */
+/*==========================< FUNCTION NAME >=================================
+ ||
+ ||   Function_Description:
+ ||      - polymophic method that is called to populate the current object
+ ||
+ ||   Preconditions:
+ ||      - None
+ ||      -
+ ||   Postconditions:
+ ||      - The current movie is drama is constructed, because the objects are 
+ ||     different attributes, they will split differently
+ ||      -
+ ||
+ ||   Assumptions:
+ ||      - None
+ ++===========================================================================*/
+
 void Drama::makeMovie(std::vector<std::string> array){
    // std::cout << "Make Movie for Comed called" << std::endl;
     //index 0: command | 1: qutity | 2:Director | 3: Title |4: actor and year
@@ -83,8 +155,20 @@ void Drama::makeMovie(std::vector<std::string> array){
     
 };
 
-//output operator work around, flow transition.
-void Drama::print(std::ostream& output) const{
-    //not sure why i dont just leave this method inside the abstract parent class
-        output << getYear() << " " << getTitle() << " " << getDirector() << " " << getStock();
-};
+
+/*==========================< FUNCTION NAME >=================================
+ ||
+ ||   Function_Description:
+ ||      -
+ ||
+ ||   Preconditions:
+ ||      - None
+ ||      -
+ ||   Postconditions:
+ ||      - None
+ ||      -
+ ||
+ ||   Assumptions:
+ ||      - None
+ ++===========================================================================*/
+
